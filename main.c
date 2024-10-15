b0b4235f84f6bf279cda9ebd4d8bd2f710883df4
leomuso <Pinosenx@gmail.com>
2024-10-15 18:09:24 +0200

    exercise 4.11

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    int num = 100;
-    while (num >=0 && num <= 100 && num %2 == 0) {
+    int num /*= 100*/;
+    /*while (num >=0 && num <= 100 && num %2 == 0) {
         printf("%d\n", num);
         num = num - 2;
+    }*/
+    for (num = 100; num >= 0 && num <= 100 && num %2 == 0; num-=2) {
+        printf("%d\n", num);
     }
     return 0;
 }
